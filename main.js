34717b342a45d2900e8e1c9c63db8fd4109af99d
Marc Lundgren <4291045+theobregorkpf@users.noreply.github.com>
2017-12-01 00:44:30 -0800

    vuejs

--- main.js
+++ main.js
@@ -38,75 +38,124 @@ const names = [
   'three',
 ]
 
-const getRandomKey = (hash) => {
-  const keys  = Object.keys(hash)
-  const min   = 0
-  const max   = keys.length - 1
-  const index = Math.floor(Math.random() * (max - min + 1)) + min
-
-  return hash[index]
+const getRandomInteger = ({max = 10, min = 0}) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
-Vue.component('card-item', {
-  props: ['name'],
-  template: `<div class="card animate bg-${name}"></div>`
-})
-var vm = new Vue({
-  el: '#app',
-  data: {
-    // names,
-    // template: '<p id="app">Hello</p>',
-    count: 3,
-  },
-})
-/*
-var vm = new Vue({
-  el: '#app',
-  data: {
-    // names,
-    // template: '<p id="app">Hello</p>',
-    count: 3,
-  },
-  computed: {
-    wat() {
-      return 1
-    },
-    namesHash() {
-      return this.names().reduce((hash, item) => hash[item] = item, {})
-    },
-    names() {
-      // debugger
-      let count = this.count
-      console.log({count})
-      let namesHashCopy = Object.assign({}, this.namesHash())
-      let selectedNamesHash = {}
+const shuffle = (array) => {
+  const arrayCopy = array.slice(0) // avoid mutation
 
-      // while (count > 0) {
-      //   const key  = getRandomKey(namesHashCopy)
-      //   const name = key
-      //
-      //   delete namesHashCopy[key]
-      //
-      //   selectedNames.push(key)
-      //
-      //   count = count - 1
-      // }
+  var currentIndex = arrayCopy.length, temporaryValue, randomIndex
 
-      // return Object.keys(selectedNames)
-      return 'This and that'
-    },
-    firstCardName: function () {
-      return names[0];
-      // `this` points to the vm instance
-      // return this.message.split('').reverse().join('')
-    }
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex -= 1
+
+    // And swap it with the current element.
+    temporaryValue = arrayCopy[currentIndex]
+    arrayCopy[currentIndex] = arrayCopy[randomIndex]
+    arrayCopy[randomIndex] = temporaryValue
   }
+
+  return arrayCopy
+}
+
+/*
+Vue.component('todo-item', {
+  template: '\
+    <li>\
+      {{ title }}\
+      <button v-on:click="$emit(\'remove\')">X</button>\
+    </li>\
+  ',
+  props: ['title']
 })
 */
 
-// var app = new Vue({
-//   el: '#app',
-//   data: { names },
-// })
+Vue.component('card-item', {
+  __template: '<div class="card"> <div :class="className"></div> </div>',
+  _template: '<div :class="className"></div>',
+  __template: '<img src="/assets/images/{{name}}.png" alt="" />',
+  template: '<div class="card"><img v-bind:src="url" /></div>',
+  props: ['name'],
+  methods: {
+    reverseMessage: function () {
+      return 'fuckyou'
+    },
+  },
+  computed: {
+    className() {
+      return `bg bg-${this.name}`;
+    },
+    url() {
+      return `/assets/images/${this.name}.png`
+    }
+  },
+})
 
-// console.log({app})
+new Vue({
+  el: '#todo-list-example',
+  data: {
+    names,
+    nextTodoId: 4,
+    show: true,
+  },
+  computed: {
+    selectedNames: function () {
+      return this.threeRandomCards().slice(0)
+      const shuffledNames = shuffle(this.names)
+
+      console.log({shuffledNames})
+
+      return [
+        {id: 0, name: shuffledNames[0] },
+        {id: 1, name: shuffledNames[1] },
+        {id: 2, name: shuffledNames[2] },
+      ]
+    }
+  },
+  methods: {
+    reverseMessage: function () {
+      return 'fuckyou'
+    },
+    threeRandomCards: function() {
+      const shuffledNames = shuffle(this.names)
+
+      console.log({shuffledNames})
+
+      return [
+        {id: 0, name: shuffledNames[0] },
+        {id: 1, name: shuffledNames[1] },
+        {id: 2, name: shuffledNames[2] },
+      ]
+    },
+    addNewTodo: function () {
+      this.todos.push({
+        id: this.nextTodoId++,
+        title: this.newTodoText
+      })
+      this.newTodoText = ''
+    },
+    reshuffle() {
+      debugger
+      this.$forceUpdate();
+      // vm.$forceUpdate();
+    },
+    rerender(){
+      this.$forceUpdate();
+      console.log('rerender...')
+        this.show = false
+        this.$nextTick(() => {
+            this.show = true
+            console.log('re-render start')
+            this.$nextTick(() => {
+                console.log('re-render end')
+                this.$forceUpdate();
+            })
+        })
+    }
+  }
+})
